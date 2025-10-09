878f672076a69fe8fdd660ef92201273fd20ce5d
Your Name <your.email@example.com>
2025-10-09 11:43:37 +0100

    Change base path to root domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bbfl-frontier.github.io',
-  base: '/bbfl-website',
+  base: '/',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
